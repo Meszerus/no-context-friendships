f6c89c82db821d9ec9ca03ba739a01f94216567f
Meszerus <meszerus@gmail.com>
2021-04-15 20:42:26 +0100

    okay actually fixing it this time

--- script/model/randomise.js
+++ script/model/randomise.js
@@ -1,5 +1,5 @@
 const randomiseQuote = () => {
     var randomPerson = Math.floor(Math.random() * quotes.length);
     var randomQuote = Math.floor(Math.random() * quotes[randomPerson].length);
-    $("#quote").text(quotes[randomQuote]);
+    $("#quote").text(quotes[randomPerson][randomQuote]);
 };
