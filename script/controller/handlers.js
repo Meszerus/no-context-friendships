fdffff532d5501b1d2a0e6ba0b5ca6c656f2a96d
Meszerus <meszerus@gmail.com>
2021-04-16 19:02:22 +0100

    adding ability to add new quotes

--- script/controller/handlers.js
+++ script/controller/handlers.js
@@ -1,3 +1,9 @@
 $("#randomise").click(() => {
     randomiseQuote();
 });
+
+$("#new").click(() => {
+    const emailUser = "deicideisfun";
+    const emailDomain = "gmail.com"
+    window.open(`mailto:${emailUser}@${emailDomain}?subject=new-quote`);
+});
